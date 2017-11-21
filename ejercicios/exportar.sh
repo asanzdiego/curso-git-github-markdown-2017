78d9d8b62641436b2812d998fc5b99183217c040
Adolfo Sanz De Diego <asanzdiego@gmail.com>
2017-11-21 11:16:35 +0100

    exportación de enunciados y soluciones de los ejercicios

---
+++ ejercicios/exportar.sh
@@ -0,0 +1,7 @@
+pandoc -w odt -o ./exportar/git-github-markdown-ejercicios-01-basico.odt git-github-markdown-ejercicios-01-basico.md
+
+pandoc -w odt -o ./exportar/git-github-markdown-ejercicios-01-basico-solucion.odt git-github-markdown-ejercicios-01-basico-solucion.md
+
+pandoc -w odt -o ./exportar/git-github-markdown-ejercicios-02-avanzado.odt git-github-markdown-ejercicios-02-avanzado.md
+
+pandoc -w odt -o ./exportar/git-github-markdown-ejercicios-02-avanzado-solucion.odt git-github-markdown-ejercicios-02-avanzado-solucion.md
